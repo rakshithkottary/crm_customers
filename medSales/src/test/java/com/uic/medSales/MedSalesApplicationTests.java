573b0bd1fff034b6836ca067c299d3c76a393660
rakshithkottary <54068358+rakshithkottary@users.noreply.github.com>
2023-11-10 11:47:10 +0530

    Adding medSales server application

---
+++ medSales/src/test/java/com/uic/medSales/MedSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uic.medSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
